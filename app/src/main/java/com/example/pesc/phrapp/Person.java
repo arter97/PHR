f58835832f03380cad55888bfc86c5b80065a2b6
yeongsaem <ahnsaem90@naver.com>
2016-09-17 15:19:23 +0900

    json and http login person class

---
+++ app/src/main/java/com/example/pesc/phrapp/Person.java
@@ -0,0 +1,16 @@
+package com.example.pesc.phrapp;
+
+import java.util.Date;
+
+/**
+ * Created by saemy on 2016-09-17.
+ */
+public class Person
+{
+    public static String userid;
+    public static String password;
+    public static Date birth=new Date();
+    public static int sex;
+    public static String name;
+    public static String phonenumber;
+}
